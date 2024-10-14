22a387c449e4c32e35dd2f8e38e4c08129cb7fc2
himo-tech <hmounir@himo-tech.ca>
2024-10-14 12:13:57 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ function loadCSVFromURL(url) {
 
 // Function to load and parse CSV data
 async function loadCSVData() {
-    const fileName = 'https://raw.githubusercontent.com/himo-tech/cmam-prayer-time/refs/heads/main/2024-prayer-time.csv?token=GHSAT0AAAAAACX5HQTOMLNYCAB6OZ2KUFQ4ZYMX72Q';
+    const fileName = 'https://raw.githubusercontent.com/himo-tech/cmam-prayer-time/refs/heads/main/2024-prayer-time.csv';
     try {
         const data = await loadCSVFromURL(fileName);
         console.log('CSV data:', data);

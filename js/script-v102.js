bc293b6a57883d5471134b19f7ded43506a01984
himo-tech <hmounir@himo-tech.ca>
2026-04-09 13:46:32 -0400

    Update Zuhr iqamah time from '13:30' to '13:10'

--- js/script-v102.js
+++ js/script-v102.js
@@ -2,7 +2,7 @@
 let prayerTimes = null;
 let currentDate = new Date(); // Machine local time
 const currentYear = currentDate.getFullYear().toString();
-const iqamahTime = {fajr: 20, zuhr: 15, asr: 15, maghrib: 5, isha: 10};
+const iqamahTime = {fajr: 20, zuhr: 10, asr: 15, maghrib: 5, isha: 10};
 
 // Flag to prevent multiple simultaneous retry attempts
 let isRetrying = false;
@@ -111,8 +111,8 @@ function ishaIqamahTime(currentPrayerTime, currentIqamahTime) {
 // Iqamah logic for Zuhr
 function zuhrIqamahTime(currentPrayerTime, currentIqamahTime) {
     const iqamahTimeMinutes = timeToMinutes(currentIqamahTime);
-    const minimumIqamahTime = timeToMinutes('13:30');
-    return (iqamahTimeMinutes < minimumIqamahTime) ? '13:30' : currentIqamahTime;
+    const minimumIqamahTime = timeToMinutes('13:10');
+    return (iqamahTimeMinutes < minimumIqamahTime) ? '13:10' : currentIqamahTime;
 }
 
 // Global Iqamah Selector
